6452b84427950baa2fa90cf147ca5a89816fa175
傻小子QZ <609435061@qq.com>
2017-08-11 16:28:41 +0800

    no commit message

---
+++ src/test.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace test;
+/**
+ * Created by PhpStorm.
+ * User: fly
+ * Date: 17/8/11
+ * Time: 11:05
+ */
+class test {
+
+
+    public static function echoOk()
+    {
+        echo "ok";
+    }
+}
